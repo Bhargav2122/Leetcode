0f548cfd99985479f29aaa1a0c4812a9a794e305
Bhargav2122 <balabhargavsurepalli@gmail.com>
2024-09-09 23:08:49 +0530

    Time: 1 ms (99.43%) | Memory: 56.5 MB (97.54%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -3,15 +3,13 @@ class Solution {
         //The optimal appraoch is KADANE'S ALGO
         //The Intuition behind is simple if the sum is -ve. if (sum < 0)
         //we get least sum so we ignore -ve sum for that we set sum = 0.
+        // TC: O(N) SC : O(1)
 
         int maxSum = Integer.MIN_VALUE;
         int sum = 0;
         for(int i = 0; i < nums.length; i++) {
               sum += nums[i];
-              if(maxSum < sum) {
-                maxSum = sum;
-              }
-
+              maxSum = sum > maxSum ? sum : maxSum;
             if(sum < 0){
                 sum = 0;
             }
@@ -24,7 +22,7 @@ class Solution {
 
 The brute force approach that comes to my mind is
  iterate through array and keep another variable j it starts from i take the sum 
- we simply return maxSum 
+ we simply return maxSum . TC:O(~N*2)
 int maxSum = Integer.MIN_VALUE;
         for(int i =0; i < nums.length; i++) {
               int sum =0;
