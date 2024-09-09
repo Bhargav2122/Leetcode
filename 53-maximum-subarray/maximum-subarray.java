ac288158c4e27f7b99f131532c1a298e8295ab39
Bhargav2122 <balabhargavsurepalli@gmail.com>
2024-09-09 23:06:14 +0530

    Time: 1 ms (99.43%) | Memory: 56.9 MB (72.36%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        //The optimal appraoch is KADANE'S ALGO
+        //The Intuition behind is simple if the sum is -ve. if (sum < 0)
+        //we get least sum so we ignore -ve sum for that we set sum = 0.
+
+        int maxSum = Integer.MIN_VALUE;
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++) {
+              sum += nums[i];
+              if(maxSum < sum) {
+                maxSum = sum;
+              }
+
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+        return maxSum;
+          
+    }
+}
+/*
+
+The brute force approach that comes to my mind is
+ iterate through array and keep another variable j it starts from i take the sum 
+ we simply return maxSum 
+int maxSum = Integer.MIN_VALUE;
+        for(int i =0; i < nums.length; i++) {
+              int sum =0;
+            for(int j =i; j < nums.length; j++){
+               sum += nums[j];
+               if(maxSum < sum) {
+                maxSum = sum;
+               }
+            }
+        }
+        return maxSum;
+
+*/
